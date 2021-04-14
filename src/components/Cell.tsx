f8dfc61d39f151c3b268721b39469c9f44e47072
hoxirious <giahy33@gmail.com>
2021-04-13 23:47:59 -0600

    added state for player

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,11 +1,23 @@
+import React, { useState } from 'react';
 import './Style.css';
 
 function Cell() {
+    const [player,setPlayer] = useState('');
+
+    const playerHandle = (click:boolean) =>{
+        if(click === true)
+        {
+            return 'X';
+        } 
+        else return 'O';
+    }
+
     return (
-        <div>
-            <button className="square"></button>
+        <div>            
+            <button className="square" onClick={() =>setPlayer(playerHandle(false))}>{player}</button>
         </div>
     )
 }
 
 export default Cell
+
